1ef50711e7ecdc31d9c0d7a98ff11af85d87b820
Vincent Link <mail@linkvt.de>
2025-09-30 15:20:24 +0200

    chore: add ci and update deps

---
+++ release.config.mjs
@@ -0,0 +1,26 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
+    [
+      "@semantic-release/npm",
+      {
+        pkgRoot: "eslint-plugin-pulumi",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "eslint-plugin-pulumi/package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
